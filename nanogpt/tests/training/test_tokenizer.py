7bc96e4094050e6a2c139b394645376616bd474b
fcuni <francesc.cunillera@gmail.com>
2024-08-12 15:56:35 +0100

    fix tests

--- nanogpt/tests/training/test_tokenizer.py
+++ nanogpt/tests/training/test_tokenizer.py
@@ -3,10 +3,11 @@ import pytest
 from nanogpt.training.tokenizer import CharTokenizer, HuggingFaceTokenizer
 
 
-@pytest.mark.parametrize("seq_len, vocab_size, phrase", [(5, 256, "hello"), (10, 256, "hello")])
+@pytest.mark.parametrize("seq_len, vocab_size, phrase", [(5, 256, ["hello"]), (10, 256, ["hello"])])
 def test_char_tokenizer_returns_single_character_tokens(seq_len, vocab_size, phrase):
     tokenizer = CharTokenizer(seq_len=seq_len, vocab_size=vocab_size)
-    tokens = tokenizer.encode(phrase)
+    tokenized_input = tokenizer.encode(phrase)
+    tokens = tokenized_input.get("input_ids")
 
     # expected encoding length is seq_len
     assert tokens.size(-1) == seq_len, f"Expected length to be {seq_len=}, got {tokens.size(-1)}"
@@ -15,14 +16,15 @@ def test_char_tokenizer_returns_single_character_tokens(seq_len, vocab_size, phr
     # if we slice out the padding tokens, we should have the original phrase
     pad_token_id = tokenizer.pad_token_id
     tokens = tokens[tokens != pad_token_id].unsqueeze(0)
-    sentence_decoded = "".join(tokenizer.decode(tokens))
+    sentence_decoded = tokenizer.decode(tokens)
     assert sentence_decoded == phrase, f"Expected {phrase=}, got {sentence_decoded=}"
 
 
 def test_char_tokenizer_handles_oov_tokens():
     tokenizer = CharTokenizer(seq_len=5, vocab_size=80)
-    input_text = "h"
-    tokens = tokenizer.encode(input_text)
+    input_text = ["h"]
+    tokenized_input = tokenizer.encode(input_text)
+    tokens = tokenized_input.get("input_ids")
 
     oov_token = tokenizer.oov_token
     assert tokens[0, 0] == oov_token, f"Expected {oov_token=}, got {tokens[0, 0]}"
